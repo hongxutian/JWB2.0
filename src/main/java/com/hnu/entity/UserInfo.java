85252b077d38b9d6b399f0ab86f83e54a5b010d8
xh-hnu <xuhe@hnu.edu.cn>
2020-02-29 20:02:06 +0800

    添加mapping映射文件

--- src/main/java/com/hnu/entity/UserInfo.java
+++ src/main/java/com/hnu/entity/UserInfo.java
@@ -1,4 +1,140 @@
 package com.hnu.entity;
 
 public class UserInfo {
+
+    private int id;
+    private int uType;
+    private int openId;
+    private String nickName;
+    private String avatarUrl;
+    private String genver;
+    private String nation;
+    private String privince;
+    private String city;
+    private String district;
+    private String street;
+    private String streetNumber;
+    private String storeName;
+    private float lon;
+    private float lat;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getuType() {
+        return uType;
+    }
+
+    public void setuType(int uType) {
+        this.uType = uType;
+    }
+
+    public int getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(int openId) {
+        this.openId = openId;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getGenver() {
+        return genver;
+    }
+
+    public void setGenver(String genver) {
+        this.genver = genver;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public String getPrivince() {
+        return privince;
+    }
+
+    public void setPrivince(String privince) {
+        this.privince = privince;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
 }
---
+++ src/main/java/com/hnu/repository/ResourceRepository.java
@@ -0,0 +1,4 @@
+package com.hnu.repository;
+
+public class ResourceRepository {
+}
