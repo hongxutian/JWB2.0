6e1a346fd611468448051d0b9c003263449f6e8a
xh-hnu <xuhe@hnu.edu.cn>
2020-02-29 12:54:39 +0800

    创建项目

---
+++ src/main/java/com/hnu/JWBApplication.java
@@ -0,0 +1,12 @@
+package com.hnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JWBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JWBApplication.class, args);
+    }
+}
