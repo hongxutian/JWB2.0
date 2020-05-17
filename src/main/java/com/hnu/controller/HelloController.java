ad19d3ba637e082ff7c388a3dfcc99cab7f88d30
hongxutian <45614569+hongxutian@users.noreply.github.com>
2020-05-17 11:09:05 +0800

    针对新增的生活服务做出的修改

--- src/main/java/com/hnu/controller/HelloController.java
+++ src/main/java/com/hnu/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.hnu.controller;
 
-import com.hnu.utils.AppJWTUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,19 +11,6 @@ public class HelloController {
     @ApiOperation(value = "测试接口")
     public String hello() {
         return "hello, JWB";
-    }
-
-    @GetMapping("/t/gettoken")
-    @ApiOperation(value = "测试接口，测试JWT，获取token，30分钟有效")
-    public String getToken(){
-        String content = Long.toString(System.currentTimeMillis());
-        return AppJWTUtil.createToken(content,0,0,30,AppJWTUtil.sec);
-    }
-
-    @GetMapping("/t/verifytoken")
-    @ApiOperation(value = "测试接口，测试JWT，验证token")
-    public boolean verifyToken(@RequestParam String token){
-        return AppJWTUtil.verifyToken(token,AppJWTUtil.sec);
     }
 
 }
